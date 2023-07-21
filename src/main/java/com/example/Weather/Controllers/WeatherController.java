9364a48836c536a428cbdb4e4e520f1bb4647f1a
Naza1re <mcarim@mail.ru>
2023-07-22 00:59:06 +0300

    change html and replace realization

--- src/main/java/com/example/Weather/Controllers/WeatherController.java
+++ src/main/java/com/example/Weather/Controllers/WeatherController.java
@@ -24,30 +24,12 @@ public class WeatherController {
 
     @PostMapping("/weather")
     public String getWeather(@RequestParam("city") String city, Model model) throws JSONException {
+            APIConnection(city);
 
-        String output = getUrlContent("http://api.openweathermap.org/data/2.5/find?q=" + city + "&type=like&APPID=97873f87fc1b4927356aa0b926698655");
-        System.out.println(output);
-
-
-        JSONObject obj = new JSONObject(output);
-        JSONArray list = obj.getJSONArray("list");
-
-        for (int i = 0; i < list.length(); i++) {
-            JSONObject cityObj = list.getJSONObject(i);
-            JSONObject mainObj = cityObj.getJSONObject("main");
-
-
-            int  temperature = (int) mainObj.getDouble("temp");
-            int feelsLike = (int) mainObj.getDouble("feels_like");
-            int pressure =  mainObj.getInt("pressure");
-
-
-
-            model.addAttribute("temperature", fromKelvinToCels(temperature));
-            model.addAttribute("feelsLike", fromKelvinToCels(feelsLike));
-            model.addAttribute("pressure", fromPhaToMm(pressure)+ " мм.рт.ст");
+            model.addAttribute("temperature", fromKelvinToCels(takingAPIForTemp(city)));
+            model.addAttribute("feelsLike", fromKelvinToCels(takingAPIForFeels(city)));
+            model.addAttribute("pressure", fromPhaToMm(takingAPIForPressure(city))+ " мм.рт.ст");
             model.addAttribute("city",city);
-        }
 
         return "weather";
 
--- src/main/java/com/example/Weather/TakingWeatherFromAPI/TakingWeather.java
+++ src/main/java/com/example/Weather/TakingWeatherFromAPI/TakingWeather.java
@@ -1,5 +1,9 @@
 package com.example.Weather.TakingWeatherFromAPI;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -21,19 +25,64 @@ public class TakingWeather {
             }
             bufferedReader.close();
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         return content.toString();
     }
 
-    public static int fromKelvinToCels(int temperature) {
-        return temperature - 273;
+    public static String APIConnection(String city) throws JSONException {
+         return getUrlContent("http://api.openweathermap.org/data/2.5/find?q=" + city + "&type=like&APPID=97873f87fc1b4927356aa0b926698655");
     }
 
-    public static double fromPhaToMm(int pressure) {
-        return (int)(pressure * 0.75006375541921);
+    public static int takingAPIForTemp(String city) throws JSONException {
+        JSONObject obj = new JSONObject(APIConnection(city));
+        JSONArray list = obj.getJSONArray("list");
+        JSONObject mainObj = null;
+
+        for (int i = 0; i < list.length(); i++) {
+            JSONObject cityObj = list.getJSONObject(i);
+             mainObj = cityObj.getJSONObject("main");
+
+        }
+
+        return (int) mainObj.getDouble("temp");
+
     }
 
+    public static int takingAPIForFeels(String city) throws JSONException {
+        JSONObject obj = new JSONObject(APIConnection(city));
+        JSONArray list = obj.getJSONArray("list");
+        JSONObject mainObj = null;
 
+        for (int i = 0; i < list.length(); i++) {
+            JSONObject cityObj = list.getJSONObject(i);
+            mainObj = cityObj.getJSONObject("main");
 
+        }
+
+        return (int) mainObj.getDouble("feels_like");
+
+    }
+
+    public static int takingAPIForPressure(String city) throws JSONException {
+        JSONObject obj = new JSONObject(APIConnection(city));
+        JSONArray list = obj.getJSONArray("list");
+        JSONObject mainObj = null;
+
+        for (int i = 0; i < list.length(); i++) {
+            JSONObject cityObj = list.getJSONObject(i);
+            mainObj = cityObj.getJSONObject("main");
+
+        }
+
+        return (int) mainObj.getDouble("pressure");
+
+    }
+    public static int fromKelvinToCels(int temp){
+        return temp - 273;
+    }
+
+    public static double fromPhaToMm(int pressure){
+        return (int) (pressure * 0.75006375541921);
+    }
 }
